c063b79e4d522ab794c9f8b8b882722561af23ca
TobiasChia <62996331+TobiasChia@users.noreply.github.com>
2025-03-20 00:21:04 +0800

    Update

--- main.py
+++ main.py
@@ -1,45 +1,100 @@
 import os
 from openai import AsyncOpenAI
-from agents import Agent, Runner, function_tool, set_default_openai_api, set_default_openai_client, set_tracing_disabled
+import simplejson as json
+from agents import Agent, Runner, FileSearchTool, WebSearchTool, ComputerTool, function_tool, set_default_openai_api, set_default_openai_client, set_tracing_disabled
 from dotenv import load_dotenv
-from layer.common import load_multiple_json_files, save_to_json_file
+import asyncio
 
 load_dotenv()  
 
 OPENAI_BASE_URL="https://api.openai.com/v1"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPEN_AI_MODEL = "gpt-4o"
 DEEPSEEK_BASE_URL="https://api.deepseek.com"
 DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 GROK_BASE_URL = "https://api.x.ai/v1"
 GROK_API_KEY = os.getenv("GROK_API_KEY")
+GROK_MODEL = "grok-2-1212"
 CLAUDE_BASE_URL = "https://api.anthropic.com/v1"
 CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY")
 GEMINI_BASE_URL = "https://api.gemini.com/v1"
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-client = AsyncOpenAI(base_url=GROK_BASE_URL, api_key=GROK_API_KEY)
+client = AsyncOpenAI(base_url=OPENAI_BASE_URL, api_key=OPENAI_API_KEY)
 
 set_default_openai_client(client=client, use_for_tracing=False)
 set_default_openai_api("chat_completions")
 set_tracing_disabled(disabled=True)
 
-json_files = ['data/agent.json']
+MODEL = OPEN_AI_MODEL
 
-data = load_multiple_json_files(json_files)
+@function_tool
+def upsert_agent(name: str, instructions: str):
+    with open('data\agent.json', 'r', encoding='utf-8') as f:
+        agents = json.load(f)
 
-HR = Agent(name="HR", instructions="", model="",handoffs=[Manager])
+    if not isinstance(agents, list):
+        agents = []
 
-Manager = Agent(name="Manager", instructions="", model="",handoffs=[HR])
+    agent_exists = False
+    for i, agent in enumerate(agents):
+        if agent["name"] == name:
+            agents[i] = {
+                "name": name,
+                "instructions": instructions,
+                "model": MODEL
+            }
+            agent_exists = True
+            break
 
-task = ""
+    if not agent_exists:
+        new_agent = {
+            "name": name,
+            "instructions": instructions,
+            "model": MODEL
+        }
+        agents.append(new_agent)
 
-result = Runner.run(Manager, input=task)
+    with open('data/agent.json', 'w', encoding='utf-8') as f:
+        json.dump(agents, f, ensure_ascii=False, indent=4)
 
-save_to_json_file(result, 'data/agent.json')
+    return agents
 
-agent = {}
-for key, value in data["agent"].items():
-    agent[key] = Agent(name=value['name'], instructions=value['instructions'], model=value['model'])
+def load_agents():
+    with open('data/agent.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    agents = []
+    for item in data:
+        agent = Agent(
+            name=item["name"],
+            instructions=item["instructions"],
+            model=MODEL,
+        )
+        agents.append(agent)
+    return agents
+
+def find_agent_by_name(agents, name):
+    for agent in agents:
+        if agent.name == name:
+            return agent
+    return None
+
+agents = load_agents()
+find_agent_by_name(agents, "HR").tools = [upsert_agent]
+
+Manager = Agent(name="Manager", instructions="我是Manager, 负责调用一切资源协助客户完成需求", model=MODEL,handoffs=agents)
+
+user_input = input()
+while user_input.lower() != "exit":
+    result = Runner.run_sync(Manager, user_input.strip())
+    print(result.final_output)
+    user_input = input()
+
+
+
+
+
 
 
 
