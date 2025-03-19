8c1760eac878b02553169e9ccf54a1860f6a6445
TobiasChia <62996331+TobiasChia@users.noreply.github.com>
2025-03-19 08:14:00 +0800

    Commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
-import layer.common as common
 from openai import AsyncOpenAI
 from agents import Agent, Runner, function_tool, set_default_openai_api, set_default_openai_client, set_tracing_disabled
 from dotenv import load_dotenv
+from layer.common import load_multiple_json_files, save_to_json_file
 
 load_dotenv()  
 
@@ -10,10 +10,12 @@ OPENAI_BASE_URL="https://api.openai.com/v1"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 DEEPSEEK_BASE_URL="https://api.deepseek.com"
 DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
-GROK_API_KEY = os.getenv("GROK_API_KEY")
 GROK_BASE_URL = "https://api.x.ai/v1"
-# CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY")
-# GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GROK_API_KEY = os.getenv("GROK_API_KEY")
+CLAUDE_BASE_URL = "https://api.anthropic.com/v1"
+CLAUDE_API_KEY = os.getenv("CLAUDE_API_KEY")
+GEMINI_BASE_URL = "https://api.gemini.com/v1"
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 client = AsyncOpenAI(base_url=GROK_BASE_URL, api_key=GROK_API_KEY)
 
@@ -21,30 +23,26 @@ set_default_openai_client(client=client, use_for_tracing=False)
 set_default_openai_api("chat_completions")
 set_tracing_disabled(disabled=True)
 
-# @function_tool
-# def get_story(agent, task):
-#     result = Runner.run_sync(agent['Ideator'], task)
-#     result = Runner.run_sync(agent['Architect'], result.final_output)
-#     return result.final_output
-
-
 json_files = ['data/agent.json']
 
-data = common.load_multiple_json_files(json_files)
+data = load_multiple_json_files(json_files)
+
+HR = Agent(name="HR", instructions="", model="")
+
+Manager = Agent(name="Manager", instructions="", model="",tools=[function_tool("HR", HR)])
+
+result = Runner.run(Manager, input="")
+
+
+
+
+
+save_to_json_file(result, 'data/agent.json')
 
 agent = {}
 for key, value in data["agent"].items():
     agent[key] = Agent(name=value['name'], instructions=value['instructions'], model=value['model'])
 
-Task = "主题：异世界，剑与魔法"
-
-result = Runner.run_sync(agent['Ideator'], Task)
-print(result.final_output)
-
-result = Runner.run_sync(agent['Architect'], result.final_output)
-print(result.final_output)
-
-common.save_to_json_file(result.final_output, 'data/story.json')
 
 
 
